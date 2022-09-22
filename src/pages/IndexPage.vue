d55d53da5582f9265a8ded368b73d856e8a0bb20
raul.hess <raul.hess@preambulo.com.br>
2022-09-22 17:17:32 -0300

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-page class="bg-grey-1" style="font-size: 16px">
+    <q-img no-spinner src="chapters/kings-rock.png"></q-img>
+    <div class="q-px-md q-mt-xs">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nec
+      tempor ex. In at feugiat sapien, a tincidunt nibh. Sed nec mi eget elit
+      pharetra sollicitudin eget vel nisl. Ut in porta leo. Etiam ut erat eu
+      nunc feugiat blandit. Quisque tincidunt tincidunt enim, id luctus metus
+      interdum vel. Suspendisse enim augue, sodales a porttitor a, mollis at
+      turpis. Sed euismod vehicula erat, gravida varius augue venenatis nec.
+      Quisque vel erat cursus, ultrices ex vel, sollicitudin ex. Nulla elementum
+      volutpat nisl, quis consequat magna blandit sit amet. Curabitur gravida
+      augue velit, et rhoncus lacus feugiat ut. Duis efficitur nisi eros, vel
+      feugiat ante congue eget. Curabitur dictum vitae ex ut bibendum. Vivamus
+      tempus sollicitudin odio eu viverra.
+    </div>
+    <div class="q-px-md q-mt-md">
+      Vivamus rhoncus pellentesque diam. Vivamus nec rhoncus metus, eu
+      scelerisque ipsum. Aliquam faucibus, urna sit amet sollicitudin sagittis,
+      arcu est tempus ante, nec fermentum metus diam eget magna. Curabitur sed
+      erat pulvinar, finibus tortor sed, suscipit eros. Pellentesque accumsan
+      volutpat mi, a porttitor turpis hendrerit ornare. Cras id risus enim.
+      Mauris pharetra auctor augue eu vulputate. Vestibulum porttitor maximus
+      lorem. Nullam fermentum vehicula sem vitae tempor. Etiam leo augue,
+      tristique gravida massa non, tincidunt cursus tellus. Vivamus ullamcorper
+      faucibus tellus efficitur dignissim. Donec ultricies eu nibh feugiat
+      efficitur. Aliquam magna tellus, vulputate eu nibh non, condimentum
+      lobortis augue. Vivamus orci felis, mattis sed nisi id, molestie vehicula
+      diam.
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
